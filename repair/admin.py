25d233e31ba03c1a596a483b1521ab01a40aa867
UsmanGamidov <osmangamidov026@gmail.com>
2025-05-22 01:47:37 +0300

    total

--- repair/admin.py
+++ repair/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import RepairRequest
 
-# Register your models here.
+@admin.register(RepairRequest)
+class RepairRequestAdmin(admin.ModelAdmin):
+    list_display = ('car', 'user', 'date', 'time', 'status')
+    list_filter = ('status', 'date')
+    search_fields = ('car', 'user__username', 'problem')
+
+    def has_change_permission(self, request, obj=None):
+        if obj and obj.status != 'new':
+            return False
+        return super().has_change_permission(request, obj)
